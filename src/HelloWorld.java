5dbf1fe562223a5493befd29970cda197b33afbc
Peter Hollman <peterjhollman@gmail.com>
2019-07-25 09:17:58 -0500

    morning commit of exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    int myFavoriteNumber = 27;
+    String myString = "A script that talks about Java";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+
+        int x = 4;
+        x += 5;
+        System.out.println(x);
+
+        int z = 3;
+        int y = 4;
+        y *= z;
+        System.out.println(y);
+
+        int j = 10;
+        int k = 2;
+        j /= k;
+        k -= j;
+        System.out.println(j);
+        System.out.println(k);
     }
 }
