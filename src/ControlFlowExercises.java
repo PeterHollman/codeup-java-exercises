10fcf70e052d268934f6805e7b8b4eb17bf4c087
Peter Hollman <peterjhollman@gmail.com>
2019-07-25 15:43:24 -0500

    while loop java

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -14,6 +14,11 @@ public class ControlFlowExercises {
     }
 
     public static void main(String[] args) {
-        fizzBuzz(45);
+//        fizzBuzz(45);
+        int i = 5;
+        while (i <= 15) {
+            System.out.println("i is " + i);
+            i++;
+        }
     }
 }
