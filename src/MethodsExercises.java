f3fa48d00d96a25f13000c08a5402235e122ead4
Peter Hollman <peterjhollman@gmail.com>
2019-07-29 15:47:28 -0500

    finished exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -18,15 +18,18 @@ public class MethodsExercises {
     }
     public static int getInteger(int min, int max) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number between " + min + " and " + max);
+        System.out.println("Enter a number between " + min + " and " + max);
         do {
             int userInput = scanner.nextInt();
             if (userInput < min){
                 System.out.println("That's too low ");
+                System.out.println("Enter a number between " + min + " and " + max);
+
             } else if(userInput > max){
                 System.out.println("That's too high");
+                System.out.println("Enter a number between " + min + " and " + max);
+
             } else {
-                System.out.println(userInput);
                 return userInput;
             }
         } while(true);
@@ -38,6 +41,21 @@ public class MethodsExercises {
         System.out.println(Multiplication(6, 4));
         System.out.println(Division(8, 2));
         System.out.println(Modulus(5, 2));
-        getInteger(1, 10);
+
+        long theFactorial = getInteger(1,10);
+        long helpfulInt = theFactorial;
+        for(long i = 1; i < helpfulInt; i++){
+            theFactorial = theFactorial * i;
+        }
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("Would you like to continue [y/N] ");
+        String userInput2 = sc2.next();
+        boolean newConfirmation = userInput2.equals("y");
+        if(newConfirmation){
+            System.out.println("The Factorial of " + helpfulInt + " is " + theFactorial);
+        }
+
     }
 }
+
